fd32740b5e71e675739f655da42bccb762ecd344
Wong-debug <troymafa33@gmail.com>
2025-07-22 16:40:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["https://thetoolsnest.com","http://127.0.0.1:5500",
-        "http://localhost:5500"],
+  allow_origins=["*"],
   allow_methods=["*"],
   allow_headers=["*"],
 )
